99d7b65c8faca6f06c30276b38ff8f29d687b977
Nurelifkfl <nurelifkfl@gmail.com>
2021-06-09 19:58:06 +0100

    updated

--- src/test/java/com/lifeplus/runners/FailedTestRunner.java
+++ src/test/java/com/lifeplus/runners/FailedTestRunner.java
@@ -1,5 +1,19 @@
 package com.lifeplus.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/failed-html-report"},
+        features ="@target/rerun.txt",
+        glue = "com/lifeplus/step_definitions"
+)
+
+
+
 public class FailedTestRunner {
 
 
